fb1e93db0844f48504d1598c8ef1b4518cffbd46
Gabriel <mfkrgabriel@gmail.com>
2018-03-13 15:13:20 -0300

    updating without linter

--- src/album.js
+++ src/album.js
@@ -3,12 +3,12 @@ import { toJSON } from './utils';
 
 export const getAlbum = id =>
   fetch(`${API_URL}/albums/${id}`)
-    .then(toJSON)
+    .then(toJSON);
 
 export const getAlbums = ids =>
   fetch(`${API_URL}/albums/ids?${ids}`)
-    .then(toJSON)
+    .then(toJSON);
 
 export const getAlbumTracks = id =>
   fetch(`${API_URL}/albums/${id}/tracks`)
-    .then(toJSON)
+    .then(toJSON);
