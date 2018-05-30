35c14faa4cc447baa54dc5aebaf3880b25ffb75f
Gabriel Brito <mfkrgabriel@gmail.com>
2018-05-30 13:16:14 -0300

    Adapting to linter

--- src/config.js
+++ src/config.js
@@ -1,9 +1,14 @@
 const TOKEN_API = 'BQASPlc8MgaS0j_6svm0uo6w-VfcdxYrvfPIdSIjwJrEdER6vLWCbgJ1x2cb0bkuDfS8KxSFXp1quCfLK4Voag7A9n9W4jP_pbKnX5duS_-QRxgcdn8iQIdz-8wtMjF_pIi9fNY4ocAt19gE4po5zuONfmQROTiLYiQ';
 
-export const API_URL = 'https://api.spotify.com/v1';
+const API_URL = 'https://api.spotify.com/v1';
 
-export const HEADERS = {
+const HEADERS = {
   headers: {
     Authorization: `'Bearer ${TOKEN_API}'`,
   },
 };
+
+module.exports = {
+  API_URL,
+  HEADERS
+}
